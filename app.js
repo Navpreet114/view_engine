9aec71fb9fd4cf07f9b41ef23bcda4b3fbf6fde3
Navpreet114 <navpreet.singh@rdpolytech.ca>
2024-11-19 10:36:47 -0700

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/home", (req, res) => {
+    var message = "Hello World!";
+    res.render("index", {message});
+});
+
 app.listen(PORT, () => {
     console.log(`Connected to port ${PORT}`);
 });
